2988502f03150cdfd6dc9fb152f3ba2cae58544e
Shivam Agrawal <shivam301296@gmail.com>
2020-01-27 20:09:34 +0530

    Updates

---
+++ precache-manifest.725da6af3caa2517a836e1c106b4bde7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532b04b11c134d879fbb0ca7ab381de9",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "f323345d7a1782fa5053",
+    "url": "/videos/static/css/main.fa278707.chunk.css"
+  },
+  {
+    "revision": "2256cf174f0bb7a1d9e1",
+    "url": "/videos/static/js/2.1f9184ce.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/videos/static/js/2.1f9184ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f323345d7a1782fa5053",
+    "url": "/videos/static/js/main.900df6c6.chunk.js"
+  },
+  {
+    "revision": "df2018c59bd9eb74cc66",
+    "url": "/videos/static/js/runtime-main.ab2e0800.js"
+  }
+]);
